02f8e6d7b9768d80266a2d8970a57983de1e8fae
vk-otopylot <vivek.kakadiya@otopylot.com>
2025-12-03 18:22:17 +0530

    day_1_commit

--- week_1_python_basic/day1_learning/day1_practice_questions/string_formatting.py
+++ week_1_python_basic/day1_learning/day1_practice_questions/string_formatting.py
@@ -1,4 +1,27 @@
-name = 'vivek'
-age = 21
+# name = 'vivek'
+# age = 21
+#
+# print(f"hello {name}, you are {21} year old.")
 
-print(f"hello {name}, you are {21} year old.")
+a = 15
+b = 20
+
+# print(f'Addition:{a+b}')
+# print(f'Subtraction:{a-b}')
+# print(f'Multiplication:{a*b}')
+# print(f'Division:{a/b}')
+
+# msg1 = 'Addition:{}'.format(a+b)
+# msg2 = 'Subtraction:{}'.format(a-b)
+# msg3 = 'Multiplication:{}'.format(a*b)
+# msg4 = 'Division:{}'.format(a/b)
+#
+# print(msg1)
+# print(msg2)
+# print(msg3)
+# print(msg4)
+
+print('Addition:%.2f' % (a + b))
+print('Subtraction:%.2f' % (a - b))
+print('Multiplication:%.2f:' % (a * b))
+print('Division:%.2f' % (a / b))
---
+++ week_1_python_basic/day1_learning/day1_practice_questions/user_info_formatter.py
@@ -0,0 +1,5 @@
+name = input('Enter Your name: ')
+age = int(input('Enter your age: '))
+height = float(input('Enter your height in feet: '))
+
+print(f'Hi, {name} you are {age} years old and {height} feet tall.')
