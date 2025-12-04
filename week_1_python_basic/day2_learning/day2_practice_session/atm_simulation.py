f18bd01164ebfe8fe354b53fbc3f2706409624ad
vk-otopylot <vivek.kakadiya@otopylot.com>
2025-12-04 12:18:52 +0530

    day_2_commit

---
+++ week_1_python_basic/day2_learning/day2_practice_session/atm_simulation.py
@@ -0,0 +1,22 @@
+print('Welcome to the ATM. ')
+print('Enter your choice:')
+
+total_balance = 2300
+
+choice = input('1. Check Balance\n2. Deposit Money\n3. Withdraw Money\n')
+
+if choice == '1':
+    print(f'your available balance is: {total_balance}')
+
+elif choice == '2':
+    deposit_money = float(input('Enter amount to Deposit: '))
+    total_balance = total_balance + deposit_money
+    print(f'your available balance is: {total_balance}')
+
+elif choice == '3':
+    withdraw_money = float(input('Enter amount to Withdraw: '))
+    if withdraw_money > total_balance:
+        print('you do not have enough money to withdraw.')
+    else:
+        total_balance = total_balance - withdraw_money
+        print(f'your available balance is: {total_balance}')
---
+++ week_1_python_basic/day2_learning/day2_practice_session/table_generator.py
@@ -0,0 +1,4 @@
+num = int(input('Enter the number you want to generate table: '))
+
+for i in range(1,11):
+    print(f'{num} X {i} = {num * i}')
