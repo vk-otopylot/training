8314a531f30dcd8fe82365757a8caa64fa08282e
vk-otopylot <vivek.kakadiya@otopylot.com>
2025-12-04 15:11:47 +0530

    day_3_commit

---
+++ week_1_python_basic/day3_learning/string_practice_task/username_generator.py
@@ -0,0 +1,6 @@
+name = input('Enter your name: ')
+num = input('Enter your favourite number: ')
+
+username = name.lower() + num
+
+print('your username is:',username)
---
+++ week_1_python_basic/day3_learning/string_practice_task/word_count.py
@@ -0,0 +1,5 @@
+sentence = input('Enter any sentence to count words in sentence: ')
+
+words = sentence.split()
+
+print(len(words))
