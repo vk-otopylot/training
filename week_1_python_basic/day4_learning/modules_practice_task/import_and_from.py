e2224797f5d93bb74aed7998ace0abd9098981cd
vk-otopylot <vivek.kakadiya@otopylot.com>
2025-12-17 18:18:47 +0530

    day_4_commit

---
+++ week_1_python_basic/day4_learning/modules_practice_task/import_and_from.py
@@ -0,0 +1,26 @@
+# Basic Import
+
+# import math
+#
+# print(math.sqrt(64))
+# print(math.pi)
+
+# Using from
+
+# from math import sqrt, pow
+#
+# print(sqrt(81))
+# print(pow(5,3))
+
+# Rename Module (Alias)
+
+# import math as m
+#
+# print(m.sin(90))
+# print(m.ceil(2.4))
+
+# Import Specific Function
+
+from random import randint
+
+print(randint(0,100))
