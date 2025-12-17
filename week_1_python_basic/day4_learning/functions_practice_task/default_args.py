f2ac60dac331db3ae8add7801682d87c70fe1f36
vk-otopylot <vivek.kakadiya@otopylot.com>
2025-12-17 16:14:12 +0530

    day_4_commit

---
+++ week_1_python_basic/day4_learning/functions_practice_task/default_args.py
@@ -0,0 +1,13 @@
+# Default Greeting
+
+# def greet(name='guest'):
+#     return f'Hello, {name}'
+#
+# print(greet('vivek'))
+
+# Power Function
+
+def power(num, base=2):
+    return num ** base
+
+print(power(4,3))
