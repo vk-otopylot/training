cf987515e6284dc262d0443682ffc954d6c8fe9c
vk-otopylot <vivek.kakadiya@otopylot.com>
2025-12-24 11:25:09 +0530

    week_2_commit

---
+++ week_2_intermediate_python/day3_learning/collection_module_practice/practice_programs.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+# Character Frequency Counter in a String
+
+# text = 'Hello my name is vivek kakadia. i am from surat and i want to make my career bright.'
+#
+# counter = Counter(text)
+# print(counter)
+
+# Word Frequency Analyzer
+
+# text = 'Hello my name is vivek kakadia. i am from surat and i want to make my career bright.'
+# n_text = text.split()
+# res = Counter(n_text)
+#
+# print(res.values())
+
